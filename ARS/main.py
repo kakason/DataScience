09343103166ce9b8cb24f48ab383d69b0c42daa1
Evan <q910553@gmail.com>
2017-04-23 13:57:12 +0800

    load_csv_files

---
+++ ARS/main.py
@@ -0,0 +1,8 @@
+import sys
+import load_csv
+
+def main(argv):
+    load_csv.run()
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ ARS/unit_test.py
@@ -0,0 +1,37 @@
+# Test for loading of .csv
+
+import load_csv
+
+def test_load_fb_pages(a):
+    print("Test_load_fb_pages:")
+    for i in range(0, 5):
+        for j in range(0, 3):
+            print a[i][j] # the Chinese name could be printed correctly
+
+def test_load_fb_users(b):
+    print("\nTest_load_fb_users:")
+    for i in range(0, 5):
+        for j in range(0, 4):
+            print b[i][j]
+
+def test_load_fb_user_likes(c):
+    print("\nTest_load_fb_user_likes:")
+    for i in range(0, 5):
+        for j in range(0, 5):
+            print c[i][j]
+
+def test_object(a):
+    print("\nTest_object:")
+    tmp = [load_csv.fb_pages() for x in range(5)]
+    for i in range(0, 5):
+        tmp[i].id = a[i][0]
+        tmp[i].name = a[i][1]
+        tmp[i].category = a[i][2]
+        print (tmp[i].id, tmp[i].name, tmp[i].category) # could not be printed correctly
+
+def run(a, b, c):
+    test_load_fb_pages(a)
+    test_load_fb_users(b)
+    test_load_fb_user_likes(c)
+
+    test_object(a)
